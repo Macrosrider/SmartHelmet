0fa0f7af1b14e0154c0ced4944bd816f3a9d5af0
Macrosrider <Marikzub72060@gmail.com>
2017-09-26 20:01:17 +0300

    Commit №1

--- app/src/main/java/com/example/macros_pc/smarthelmet/Navigation.java
+++ app/src/main/java/com/example/macros_pc/smarthelmet/Navigation.java
@@ -28,7 +28,8 @@ public class Navigation extends FragmentActivity implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {   //this all stuff doesn't work
+    public void onMapReady(GoogleMap googleMap) {
+        //this all stuff doesn't work
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions().position(sydney)
                 .title("Marker in Sydney"));
